ee07e5d247d06ad2587e0a504538bb8478abda32
divy_kapatel <divykapatel7@gmail.com>
2026-04-04 09:14:17 +0530

    Fix CORS with multiple origins

--- src/server.ts
+++ src/server.ts
@@ -65,7 +65,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 // Rate limiting
 const limiter = rateLimit({
